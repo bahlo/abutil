7c8b84ec5171dbb86a1513610f2bba9dedab5d15
Arne Bahlo <hallo@arne.me>
2015-08-27 09:46:17 +0200

    Remove lock from Parallel
    It isn't parallel when you run each function between a mutex

--- concurrency.go
+++ concurrency.go
@@ -5,28 +5,15 @@ import (
 )
 
 // Parallel runs a given function n times concurrently
-// The function get's called with it's number (0-n) for logging purposes
 // NOTE: Please set runtime.GOMAXPROCS to runtime.NumCPU()
-func Parallel(n int, fn func(int)) {
+func Parallel(n int, fn func()) {
 	var wg sync.WaitGroup
 	wg.Add(n)
 	defer wg.Wait()
 
-	var m sync.Mutex
-	counter := 0
-
 	for i := 0; i < n; i++ {
 		go func() {
-			// Lock mutex to prevent multiple functions with the same index
-			m.Lock()
-
-			// Run function
-			fn(counter)
-
-			// Increment counter
-			counter++
-
-			m.Unlock()
+			fn()
 			wg.Done()
 		}()
 	}
--- concurrency_test.go
+++ concurrency_test.go
@@ -4,17 +4,19 @@ import (
 	"fmt"
 	"sync"
 	"testing"
-	"time"
 )
 
 func TestParallel(t *testing.T) {
 	var wg sync.WaitGroup
+	var m sync.Mutex
 
 	counter := 0
 
 	wg.Add(2)
-	go Parallel(2, func(n int) {
+	go Parallel(2, func() {
+		m.Lock()
 		counter++
+		m.Unlock()
 		wg.Done()
 	})
 	wg.Wait()
@@ -24,51 +26,18 @@ func TestParallel(t *testing.T) {
 	}
 }
 
-func TestParallelCounter(t *testing.T) {
-	var wg sync.WaitGroup
-
-	sum := 0
-	wg.Add(4)
-	go Parallel(4, func(n int) {
-		sum += n
-		wg.Done()
-	})
-
-	wg.Wait()
-
-	if sum != 6 {
-		t.Errorf("Expected sum to be %d, but got %d", 6, sum)
-	}
-}
-
-func TestParallelTiming(t *testing.T) {
-	var m sync.Mutex
-	counter := 0
-
-	go Parallel(4, func(n int) {
-		time.Sleep(time.Duration(10*n) * time.Millisecond)
-		m.Lock()
-		counter++
-		m.Unlock()
-	})
-
-	done := make(chan bool)
-	time.AfterFunc(25*time.Millisecond, func() {
-		m.Lock()
-		if counter != 2 {
-			t.Errorf("Expected counter to be %d, but got %d", 2, counter)
-		}
-		m.Unlock()
-		done <- true
-	})
-
-	<-done
-}
-
 // The most basic call
 func ParallelExamples() {
-	Parallel(4, func(n int) {
-		fmt.Print(n)
+	var m sync.Mutex
+
+	c := 0
+	Parallel(4, func() {
+		m.Lock()
+
+		fmt.Print(c)
+		c++
+
+		m.Unlock()
 	})
 
 	// Output: 0123 in any order
@@ -77,9 +46,9 @@ func ParallelExamples() {
 // If you need to pass parameters to your function, just wrap it in another
 // and call the superior function immeditately.
 func ParallelExamples_Parameters() {
-	fn := func(someParam, someOtherParam string) func(int) {
-		return func(n int) {
-			fmt.Print(n, someParam, someOtherParam)
+	fn := func(someParam, someOtherParam string) func() {
+		return func() {
+			fmt.Print(someParam, someOtherParam)
 		}
 	}
 
