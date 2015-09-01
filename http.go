76c058644f011524edb94a07cfba140b36c1e800
Arne Bahlo <hallo@arne.me>
2015-09-02 01:19:18 +0200

    Add http.go

---
+++ http.go
@@ -0,0 +1,21 @@
+package abutil
+
+// RemoteIp tries to get the remote ip and returns it or ""
+func RemoteIp(r *http.Request) string {
+	a := r.Header.Get("X-Real-IP")
+
+	if a == "" {
+		a = r.Header.Get("X-Forwarded-For")
+	}
+
+	if a == "" {
+		a = strings.SplitN(r.RemoteAddr, ":", 2)[0]
+
+		// Check localhost
+		if a == "[" {
+			a = "127.0.0.1"
+		}
+	}
+
+	return a
+}
